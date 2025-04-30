1d766216005c634171582e1a0b6f8964831ad574
ARNAB SADHUKHAN <sdknarnab13@gmail.com>
2025-04-30 08:23:46 +0530

    Change background of navbar, added word and character counter

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -6,18 +6,35 @@ export default function TextForm(props) {
         let newVar=text.toUpperCase();
         setText(newVar);
     }
+
+    const handleLoClick = ()=>{
+        //console.log("Uppercase was clicked");
+        let newVar=text.toLowerCase();
+        setText(newVar);
+    }
     const handleOnChange = (event)=>{
         //console.log("On change");
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter text here'); 
+    const [text, setText] = useState(''); 
     return (
-    <div>
+    <>
+    <div className="container">
         <div className="mb-3">
-        <h1>{props.heading}</h1> 
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="MyBox" rows="8"></textarea>
+            <h1>{props.heading}</h1> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="MyBox" rows="8"></textarea>
         </div>
-    <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+    <div className="container my-3">
+        <h2>Your text summary here</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.split(" ").length * 0.08} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+    
     )
 }
