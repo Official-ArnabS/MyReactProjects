4d99764fe4f6496fca7f881456bfc237de1eeb90
ARNAB SADHUKHAN <sdknarnab13@gmail.com>
2025-05-04 09:26:37 +0530

    Added button to clear text and to find maximum character occurence

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,21 +1,58 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
+
+    //Function to turn a text area input into upper case
     const handleUpClick = ()=>{
         //console.log("Uppercase was clicked");
         let newVar=text.toUpperCase();
         setText(newVar);
     }
 
+    //Function to turn a text area input into lower case
     const handleLoClick = ()=>{
         //console.log("Uppercase was clicked");
         let newVar=text.toLowerCase();
         setText(newVar);
     }
+
+    //Function to find maximum characters in a text area and its count
+    const handleGetMaxCharacterClick = ()=>{
+        //console.log("GetMaxCharacter was clicked");
+        let oldText=text;
+        let trimmedText=text.trim();
+        let charFrequency = {};
+            for (let char of trimmedText) {
+                if (charFrequency[char]) {
+                    charFrequency[char]++;
+                } else {
+                    charFrequency[char] = 1;
+                }
+            }
+            let maxFreq = 0;
+            let maxFreqChar = '';
+            for (let char in charFrequency) {
+                if (charFrequency[char] > maxFreq) {
+                    maxFreq = charFrequency[char];
+                    maxFreqChar = char;
+                }
+            }
+        setText(oldText+"\n"+"Maximum occurence found for character: '"+maxFreqChar+"' with count: "+maxFreq);    
+    }
+    
+    //Function to set changes made in the text area to state value
     const handleOnChange = (event)=>{
         //console.log("On change");
         setText(event.target.value);
     }
+
+    //Function to set changes made in the text area to state value
+    const handleClearClick = (event)=>{
+        //console.log("Clear click");
+        setText('');
+    }
+
+    //Base definition of TextForm function
     const [text, setText] = useState(''); 
     return (
     <>
@@ -26,6 +63,8 @@ export default function TextForm(props) {
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
+        <button className="btn btn-primary mx-2" onClick={handleGetMaxCharacterClick}>Get character with maximum occurence</button>
     </div>
     <div className="container my-3">
         <h2>Your text summary here</h2>
