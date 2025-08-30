4d9b3ccd74efccf06bef78b6f74e94361deda4b1
Arnab.Sadhukhan@outlook.com <Arnab.Sadhukhan@outlook.com>
2025-08-30 22:20:02 +0530

    Dismissable alerts added in textform utils App

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -3,25 +3,43 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
   const [mode,setMode]=useState('light');
+  const [alert,setAlert]=useState(null);
+
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
   const toggleMode = ()=>{
     if(mode==='dark'){
       setMode('light');
       document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled","success");
     }
     else{
       setMode('dark');
       document.body.style.backgroundColor='#042743';
+      showAlert("Dark mode has been enabled","success");
     }
 
   }
+
   return (
     <div>
     <Navbar title='TextUtils' aboutText='About' mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container my-3"> 
-    <TextForm heading='Enter your text to analyze below' mode={mode}/>
+    <TextForm showAlert={showAlert} heading='Enter your text to analyze below' mode={mode}/>
     {/*<About/>*/}  
     </div>
     </div>
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,89 +1,93 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
 
     //Function to turn a text area input into upper case
-    const handleUpClick = ()=>{
+    const handleUpClick = () => {
         //console.log("Uppercase was clicked");
-        let newVar=text.toUpperCase();
+        let newVar = text.toUpperCase();
         setText(newVar);
+        props.showAlert("Text converted to uppercase successfully", "success");
     }
 
     //Function to turn a text area input into lower case
-    const handleLoClick = ()=>{
+    const handleLoClick = () => {
         //console.log("Uppercase was clicked");
-        let newVar=text.toLowerCase();
+        let newVar = text.toLowerCase();
         setText(newVar);
+        props.showAlert("Text converted to lowercase successfully", "success");
     }
 
     //Function to find maximum characters in a text area and its count
-    const handleGetMaxCharacterClick = ()=>{
+    const handleGetMaxCharacterClick = () => {
         //console.log("GetMaxCharacter was clicked");
-        let oldText=text;
-        let trimmedText=text.trim();
+        let oldText = text;
+        let trimmedText = text.trim();
         let charFrequency = {};
-            for (let char of trimmedText) {
-                if (charFrequency[char]) {
-                    charFrequency[char]++;
-                } else {
-                    charFrequency[char] = 1;
-                }
+        for (let char of trimmedText) {
+            if (charFrequency[char]) {
+                charFrequency[char]++;
+            } else {
+                charFrequency[char] = 1;
             }
-            let maxFreq = 0;
-            let maxFreqChar = '';
-            for (let char in charFrequency) {
-                if (charFrequency[char] > maxFreq) {
-                    maxFreq = charFrequency[char];
-                    maxFreqChar = char;
-                }
+        }
+        let maxFreq = 0;
+        let maxFreqChar = '';
+        for (let char in charFrequency) {
+            if (charFrequency[char] > maxFreq) {
+                maxFreq = charFrequency[char];
+                maxFreqChar = char;
             }
-        setText(oldText+"\n"+"Maximum occurence found for character: '"+maxFreqChar+"' with count: "+maxFreq);    
+        }
+        setText(oldText + "Maximum occurence found for character: '" + maxFreqChar + "' with count: " + maxFreq);
     }
-    
+
     //Function to set changes made in the text area to state value
-    const handleOnChange = (event)=>{
+    const handleOnChange = (event) => {
         //console.log("On change");
         setText(event.target.value);
     }
 
     //Function to set changes made in the text area to state value
-    const handleClearClick = (event)=>{
+    const handleClearClick = (event) => {
         //console.log("Clear click");
         setText('');
+        props.showAlert("Text cleared successfully", "success");
     }
 
     //Function to remove extra spaces
-    const handleExtraSpaces = (event)=>{
+    const handleExtraSpaces = (event) => {
         //console.log("Clear click");
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "))
+        props.showAlert("Extra spaces removed successfully", "success");
     }
 
     //Base definition of TextForm function
-    const [text, setText] = useState(''); 
+    const [text, setText] = useState('');
     return (
-    <>
-    <div className="container" style={{color:props.mode==='dark'?'white':'#042743'}}>
-        <div className="mb-3">
-            <h1>{props.heading}</h1> 
-            <textarea className="form-control" value={text} onChange={handleOnChange} 
-            style={{backgroundColor:props.mode==='dark'?'#042743':'white', color:props.mode==='dark'?'white':'#042743'}}
-            id="MyBox" rows="8"></textarea>
-        </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
-        <button className="btn btn-primary mx-2" onClick={handleGetMaxCharacterClick}>Get character with maximum occurence</button>
-        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove extra spaces</button>
-    </div>
-    <div className="container my-3" style={{backgroundColor:props.mode==='dark'?'#042743':'white', color:props.mode==='dark'?'white':'#042743'}}>
-        <h2>Your text summary here</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>{text.length>0?text.split(" ").length * 0.08:0} minutes read</p>
-        <h2>Preview</h2>
-        <p>{text.length>0?text:"Enter some text in the box above to get preview"}</p>
-    </div>
-    </>
-    
+        <>
+            <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+                <div className="mb-3">
+                    <h1>{props.heading}</h1>
+                    <textarea className="form-control" value={text} onChange={handleOnChange}
+                        style={{ backgroundColor: props.mode === 'dark' ? '#042743' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }}
+                        id="MyBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
+                <button className="btn btn-primary mx-2" onClick={handleGetMaxCharacterClick}>Get character with maximum occurence</button>
+                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove extra spaces</button>
+            </div>
+            <div className="container my-3" style={{ backgroundColor: props.mode === 'dark' ? '#042743' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }}>
+                <h2>Your text summary here</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{text.length > 0 ? text.split(" ").length * 0.08 : 0} minutes read</p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Enter some text in the box above to get preview"}</p>
+            </div>
+        </>
+
     )
 }
