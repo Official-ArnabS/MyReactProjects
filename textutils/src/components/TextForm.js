f9780f1ade37e593ccee18213605dfa6f83af20e
Arnab.Sadhukhan@outlook.com <Arnab.Sadhukhan@outlook.com>
2025-08-31 11:07:00 +0530

    Updated logic for word counter

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -82,7 +82,7 @@ export default function TextForm(props) {
             </div>
             <div className="container my-3" style={{ backgroundColor: props.mode === 'dark' ? '#042743' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h2>Your text summary here</h2>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{text.length>0?text.split(" ").length:"No"} words and {text.length} characters</p>
                 <p>{text.length > 0 ? text.split(" ").length * 0.08 : 0} minutes read</p>
                 <h2>Preview</h2>
                 <p>{text.length > 0 ? text : "Enter some text in the box above to get preview"}</p>
