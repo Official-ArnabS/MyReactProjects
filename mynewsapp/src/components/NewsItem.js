db597981cb07b783a7e20aed4b5c0df87ec090c7
Arnab.Sadhukhan@outlook.com <Arnab.Sadhukhan@outlook.com>
2025-09-13 19:37:52 +0530

    Added grid to display newsitem components

--- mynewsapp/src/components/NewsItem.js
+++ mynewsapp/src/components/NewsItem.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title,description}=this.props;
     return (
-      <div>This is a news item</div>
+      <div><div className="card" style={{width: "18rem"}}>
+        <img src="https://www.thehindu.com/news/international/dvshb0/article35474284.ece/ALTERNATES/LANDSCAPE_615/thjc-DalaiLama" className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+          </div>
+      </div></div>
     )
   }
 }
