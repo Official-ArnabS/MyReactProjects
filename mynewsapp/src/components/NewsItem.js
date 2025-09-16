778c08d4474f75ed0aed10146b007d6fc56dafaa
Arnab.Sadhukhan@outlook.com <Arnab.Sadhukhan@outlook.com>
2025-09-16 11:25:33 +0530

    Added image link dynamically to the component

--- mynewsapp/src/components/NewsItem.js
+++ mynewsapp/src/components/NewsItem.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+  constructor() {
+    super()
+  }
   render() {
-    let {title,description}=this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div><div className="card" style={{width: "18rem"}}>
-        <img src="https://www.thehindu.com/news/international/dvshb0/article35474284.ece/ALTERNATES/LANDSCAPE_615/thjc-DalaiLama" className="card-img-top" alt="..."></img>
+      <div className='my-3'>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imageUrl} className="card-img-top" alt="..."></img>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
           </div>
-      </div></div>
+        </div>
+      </div>
     )
   }
 }
