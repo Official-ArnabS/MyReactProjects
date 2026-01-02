329f739898527691267e2f62f727d60d5b7666f2
Arnab.Sadhukhan@outlook.com <Arnab.Sadhukhan@outlook.com>
2026-01-02 14:14:27 +0530

    Added user addition logic for auth api endpoint

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -6,6 +6,8 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+app.use(express.json()) //Needed to take json as input
+
 //Available routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
