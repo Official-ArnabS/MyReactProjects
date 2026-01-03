fa49599c8ad9728325b4610e2f459161c5390834
Arnab.Sadhukhan@outlook.com <Arnab.Sadhukhan@outlook.com>
2026-01-03 11:04:04 +0530

    Added validation for request body fields

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 
 //Create a user using: POST "/api/auth/". Doesn't require auth
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send("Hello")
+router.post('/',[
+    body('email','Enter a valid email').isEmail(),
+    body('name','Enter a valid name').isLength({min: 3}),
+    body('password','Password must be atleast 5 characters').isLength({min: 5})
+],(req,res)=>{
+    //console.log(req.body);
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors:errors.array()})
+    }
+    else{
+        User.create({
+            name:req.body.name,
+            password:req.body.password,
+            email:req.body.email
+        }).then(user=>res.json(user)).
+        catch(err=>{
+            //console.log(err);
+            res.json({error:'Please enter unique value for email',message:err.message})
+        });
+    }
+
+    //const user = User(req.body);
+    //user.save()
+    //res.send("Hello")
 })
 
 module.exports = router;
