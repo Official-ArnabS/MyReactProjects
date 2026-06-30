18213d4e767bad04c080b1e0ca890306f617701b
HumbleFool209 <sdknarnab13@gmail.com>
2026-06-30 19:17:47 +0530

    Added logic for updating existing notes

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -44,4 +44,34 @@ body('description', 'Description must be atleast 5 characters').isLength({ min: 
     }
 })
 
+//Route 3:Updatre an existing note using POST "/api/auth/updatenote". Login required
+router.put('/updatenote/:id', fetchuser,  async (req, res) => {
+    const {title, description,tag}=req.body;
+    //Create a newNote object
+    const newNote={};
+    if(title){
+        newNote.title=title;
+    }
+    if(description){
+        newNote.description=description;
+    }
+    if(tag){
+        newNote.tag=tag;
+    }
+
+    //Find the note to be updated
+    let note = await Note.findById(req.params.id);
+    if(!note){
+        return res.status(404).send("Not found");
+    }
+    if(note.user.toString()!=req.user.id){
+        return res.status(401).send("Not allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true});
+
+    res.json({note});
+
+})
+
 module.exports = router;
